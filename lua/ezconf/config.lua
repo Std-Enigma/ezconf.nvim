da901318fb30d973e4d809d6cbecba0cb3259189
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-07-23 14:18:56 +0330

    feat(ezconf): added config module

---
+++ lua/ezconf/config.lua
@@ -0,0 +1,15 @@
+local M = {
+	autocmds = {},
+	commands = {},
+	diagnostics = {},
+	filetypes = {},
+	mappings = {},
+	on_keys = {},
+	options = {},
+	signs = {},
+	features = {
+		diagnostics_mode = 3,
+	},
+}
+
+return M
